2261a85638056994687cae0a030de74176d176b9
Denis <denisit@logsis.com>
2019-01-23 15:18:38 +0300

    save broken routing

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,14 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController extends AbstractController
 {
-     /**
-      * @Route("/lucky/number")
-      */
-    public function number()
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function home()
     {
-        echo ".kjg,kg";
+        return new Response(
+            '<html><body>Lucky number: '.rand(999, 9999).'</body></html>'
+        );
     }
+
+    /**
+     * @Route("/asdf", name="asdf")
+     * @return Response
+     */
+    public function asdf()
+    {
+        return new Response(
+            '<html><body>Lucky number: asdf</body></html>'
+        );
+    }
+
 }
