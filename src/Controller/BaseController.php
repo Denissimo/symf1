c86fc3c46682e154f49ba8015b23ebb32e5a65ee
Denis <denisit@logsis.com>
2019-03-22 09:27:52 +0300

    rm Access-Control-Allow-Origin

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -170,7 +170,7 @@ abstract class BaseController extends AbstractController
         $headers = [
             'Content-Type' => 'application/json',
             'charset' => 'utf-8',
-            'Access-Control-Allow-Origin' => 'http://editor.swagger.io',
+//            'Access-Control-Allow-Origin' => 'http://editor.swagger.io',
             'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept'
         ];
         return (new Render())->render(
